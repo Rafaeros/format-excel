0ce5b838ff2009c783b3105780b13e350dc8b5ef
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-05-02 21:35:16 -0300

    Created a class for sheet formating

--- adressformatting.py
+++ adressformatting.py
@@ -3,25 +3,38 @@ import numpy
 
 path = './address.xlsx'
 
-adress = pd.read_excel(path)
-#adress['CAIXA'] = '(' + adress['CAIXA'] + ')'
-#dataframe['ENDEREÇO'] = dataframe['ENDEREÇO'] + " " + dataframe['CAIXA']
+class formatAddress():
+  def __init__(self, path):
+    self.path = path
+    self.df = pd.read_excel(self.path)
+    self.concatdf = pd.DataFrame()
+    
+  def formatSheet(self, dataframe):
+    dataframe['CAIXA'] = '(' + dataframe['CAIXA'] + ')'
+    return dataframe
 
-def formatSheet(dataframe):
-  dataframe['CAIXA'] = '(' + dataframe['CAIXA'] + ')'
-  return dataframe
-
-def concatAdressBoxes(row):
-  if pd.notnull(row['CAIXA']) and row['CAIXA']!= '':
-    return str(row['ENDEREÇO']) + ' ' + str(row['CAIXA'])
-  else: 
-    return str(row['ENDEREÇO'])
+  def concatAdressBoxes(self, row):
+    if pd.notnull(row['CAIXA']) and row['CAIXA']!= '':
+      return str(row['ENDEREÇO']) + ' ' + str(row['CAIXA'])
+    else: 
+      return str(row['ENDEREÇO'])
   
-def concatCodes(group):
-  return ' / '.join(group['ENDEREÇO'])
+  def concatCodes(self, group):
+    return ' / '.join(group['ENDEREÇO'])
+  
+  def formatedFile(self):
+    self.df = self.formatSheet(self.df)
+    self.df['ENDEREÇO'] = self.df.apply(self.concatAdressBoxes, axis=1)
+    concatdf = self.df.groupby('CODIGO').apply(self.concatCodes).reset_index(name="ENDEREÇOS_CONCATENADOS")
+    concatdf.to_excel("EndereçoFormatadoCG.xlsx", index=False)
 
-dff = formatSheet(adress)
+
+formatFile = formatAddress(path)
+formatFile.formatedFile()
+
+
+""" dff = formatSheet(adress)
 dff['ENDEREÇO'] = dff.apply(concatAdressBoxes, axis=1)
 dfconcat = dff.groupby('CODIGO').apply(concatCodes).reset_index(name='endereços_concatenados')
 
-dfconcat.to_excel("EnderecoFormatadoCG.xlsx", index=False)
+dfconcat.to_excel("EnderecoFormatadoCG.xlsx", index=False) """
